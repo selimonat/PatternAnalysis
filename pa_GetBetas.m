2ad9d38faaa0a8abaa95b22877072884cf219152
onatselim <onatselim@gmail.com>
2015-04-16 17:05:46 +0200

    Commenting and cleaning.

--- pa_GetBetas.m
+++ pa_GetBetas.m
@@ -1,26 +1,14 @@
 function [beta]=pa_GetBetas(X,Y,N,HParam)
 %[beta]=pa_GetBetas(X,Y,N,HParam)
 %
-% Return beta values best fitting the design matrix X to observed BOLD time
-% courses in Y, after residualizing Y with the Nuissance matrix N, which will
-% be expanded with an spm-like high-pass filter matrix with parameter HPARAM.
+% Returns the weights for the design matrix X that fits best the observed
+% BOLD time-series in Y. Y and X are high-pass filtered as in spm.
 %
-% important: TR is fixed in the function body.
+% Dependency: spm_sp, spm_filter
 %
-% SPM:
-% spm_sp, spm_filter
-%
-% Project:
-% project_getdefaults
+% 
 
 
-
-
-%% plot the correlation matrix of X
-% %             figure(1);
-% %             subplot(1,2,1);
-% %             imagesc(corrcoef(X).^2,[0 1]);colorbar;
-% %             SaveFigure(sprintf('%ssub%03d/phase%02d/figures/%s.png',GetProjectRootFeargen2,subject,phase,'DesignCorrelation'));
 
 %% Get the High-pass filter by default
 K  = struct('HParam', HParam , 'row',    1:size(X,1) , 'RT',     2.02);
@@ -28,17 +16,12 @@ Y  = spm_filter(K,Y);
 %%
 % N  = [N K.X0];%combine covariates
 % N  =  N - repmat(mean(N),size(N,1),1);%and demean them
-%% plot the nuissance correlation
-%             figure(1)
-%             subplot(1,2,2);
-%             imagesc(corrcoef(M).^2,[0 1]);
-%             colorbar;
-%             SaveFigure(sprintf('%ssub%03d/phase%02d/figures/%s.png',GetProjectRootFeargen2,subject,phase,'NuissanceCorrelation'));
+
 %% first residualize with respect to Nuissance parameters.
 % Yr   = Y - N*(N\Y);
 % beta = (X\Yr)';
-X     = [X N ones(size(X,1),1)];
-xX.xKXs   = spm_sp('Set',spm_filter(K,X));       % KWX
-xX.xKXs.X = full(xX.xKXs.X);
-xX.pKX    = spm_sp('x-',xX.xKXs);                        % projector;
-beta  = xX.pKX*Y;
+DM        = [X N ones(size(X,1),1)];
+DM        = spm_filter(K,DM);
+DM        = spm_sp('Set',DM);
+DM        = spm_sp('x-',DM);                        % projector;
+beta      = DM*Y;
