c831cadba9da8624636c25b17cf3b6a9ed68b868
onatselim <onatselim@gmail.com>
2015-04-22 01:10:56 +0200

    removed useless for loops, simplified code, commented more. Some ROIs return 'Some columns have zero standard deviation.' warnings, this has to be understood

--- pa_spmbeta2cov.m
+++ pa_spmbeta2cov.m
@@ -1,6 +1,7 @@
-if ismac
-    %for me to add the project folder to path
-    addpath('/Users/onat/Documents/Code/Matlab/project_cond')
+%This script will return a similarity matrix of size
+%[condition,condition,subject,group,roi,laterality,metric]
+if ismac    
+    addpath('/Users/onat/Documents/Code/Matlab/PatternAnalysis')
 else
     addpath('/home/schenk/Documents/MATLAB/RSA')
     addpath('/home/schenk/Documents/MATLAB/RSA/project_cond')
@@ -8,27 +9,32 @@ else
     addpath('/home/schenk/Documents/MATLAB/RSA/PatternAnalysis')
 end
 %% initial variables
-rois            = 1:cond_defaults('troi')/2;%all rois; 1:46
-troi            = cond_defaults('troi')/2;%effective roi number 46
-roi_constant    = [troi 0]; %[46 0]
-vs              = cond_defaults('fov'); %[121 145 121]
-project_path    = cond_defaults('project_path');%/where the data is '/projects/cond/'
-analysis_path   = fileparts(which('cond_defaults'));%where the analysis scripts are located. '/home/schenk/Documents/MATLAB/RSA'
-[Version, ID]    = GetGit(analysis_path);
+rois              = 1:cond_defaults('troi')/2;%all rois; 1:46
+troi              = cond_defaults('troi')/2;%effective roi number 46
+roi_constant      = [troi 0]; %[46 0]
+vs                = cond_defaults('fov'); %[121 145 121]
+project_path      = cond_defaults('project_path');%/where the data is '/projects/cond/'
+analysis_path     = fileparts(which('cond_defaults'));%where the analysis scripts are located. '/home/schenk/Documents/MATLAB/RSA'
+[Version, ID]     = GetGit(analysis_path);
 group_context     = cond_defaults('group_context');%subject groups
 group_nocontext   = cond_defaults('group_nocontext');
 atlas             = cond_defaults('atlas');
 threshold         = cond_defaults('threshold');
 metrics           = cond_defaults('metrics');
-tic % start clock
+vis               = 0;%visuals
+start             = tic; % start clock
 
-%% Get the beta file list
+%% Get the beta file list.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%this part is still project specific, could be made more general in the
+%future.
 if ismac
     a = load(sprintf('%s15_04_16s4_f10_Mpain_ffMpains1_dummy/SPM.mat',project_path));
     b = load(sprintf('%s15_04_16s4_f10_Mpain_ffMpains2_dummy/SPM.mat',project_path));
-else
-%     a = load(sprintf('%s2ndlevel/15_04_21s4_f12_Mpain_ffMpain1_dummy/SPM.mat',project_path));
-%     b = load(sprintf('%s2ndlevel/15_04_21s4_f12_Mpain_ffMpain2_dummy/SPM.mat',project_path));
+else    
     a = load(sprintf('%s2ndlevel/15_04_21s4_f12_Mcue_ffMcue1_dummy/SPM.mat',project_path));
     b = load(sprintf('%s2ndlevel/15_04_21s4_f12_Mcue_ffMcue2_dummy/SPM.mat',project_path));
 end
@@ -62,63 +68,51 @@ i_cond = [i_cond(i_group == 0) i_cond(i_group == 1)];
 i_group= [i_group(i_group == 0) i_group(i_group == 1)];
 
 %sanity check
-figure(3);
-subplot(3,1,1);plot(i_sub,'o');subplot(3,1,2);plot(i_cond,'o');subplot(3,1,3);plot(i_group,'o')
-
+if vis
+    figure(3);
+    subplot(3,1,1);plot(i_sub,'o');subplot(3,1,2);plot(i_cond,'o');subplot(3,1,3);plot(i_group,'o')
+end
+%% Compute similarity
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 ts             = length(unique(i_sub))/2;%total subjects per group
 tc             = length(unique(i_cond));%total conditions
 for nroi       = rois;%run across all ROIs, and fill up the similarity matrix
-    roi = [];
     for lr = 1:2
-        fprintf('ROI: %02d, Side: %01d...\n',nroi,lr)
+        fprintf('ROI: %02d, Side: %01d, %s...\n',nroi,lr,datestr(now,'HH:MM:SS'));
         %Get roiname and voxel indices
         [dummy, mask,xyz]       = pa_GetAtlas(atlas,threshold,nroi+roi_constant(lr));
-        tvox                    = sum(mask.d(:));%total number of voxels
-        roi{nroi,lr}.name   = mask.name{1};
-        %% load voxels for all the subjects
-        raw = zeros(tvox,tc,ts,2);%init
+        tvox                    = sum(mask.d(:));%total number of voxels                        
         for groups = {group_context group_nocontext}%run across two groups
             subs = groups{1};%vectorize the cell
             s_c  = 0;%counter init
             for ns = subs
-                s_c = s_c + 1;
-                fprintf('Collecting raw data from subject %02d of %02d...\n',s_c,ts)
-                dummy         = beta_list((i_sub == ns),:);%36 conditions for subject NS
-                raw(:,:,s_c,unique(i_group(i_sub == ns)) + 1)   = spm_get_data(spm_vol(dummy),xyz)';
-            end
-        end
-        %store the matrix once as raw and then after common pattern
-        %correction
-        roi{nroi,lr}.d.raw  = raw;
-        %get the MC (this is subject specific, we don't like fanciness, we
-        %make a for loop
-        for gr = 1:2
-            for ns = 1:ts
-                common_pattern                 = mean( raw(:,:,ns,gr) ,2);%average across conditions (but not across sub or group)
-                roi{nroi,lr}.d.mc(:,:,ns,gr)   = raw(:,:,ns,gr) - repmat(common_pattern,[1 tc 1 1]);
-                %                 roi{lr}.d.mc(:,:,ns,gr)   = raw(:,:,ns,gr) - repmat(common_pattern,[1 tc 1 1]);
-            end
-        end
-    end
-    % at this stage we have a cell array ROI, which stores beta values for
-    % all conditions, subjects and groups. Left/Right has to be distributed
-    % to different cells because the number of voxels is different, so it
-    % can not be stored in a matrix.
-    %         roi.d.raw is:
-    %         [voxel,condition,subject,group]
-    %         group 1: context
-    %         group 2: nocontext
-    %% compute the similarity using different metrics
-    for lr = 1:2
-        for dtype = fieldnames(roi{nroi,lr}.d)';
-            for group = 1:2
-                for sub = 1:ts
-                    data = roi{nroi,lr}.d.(dtype{1})(:,:,sub,group); % data of one sub
-                    %
+                fprintf('.');
+                s_c = s_c + 1;                
+                dummy = beta_list((i_sub == ns),:);%36 conditions for subject NS
+                %store the matrix once as raw and then after common pattern
+                %correction
+                gr              = unique(i_group(i_sub == ns)) + 1;%+1 to avoid zero indexing
+                roi.raw         = spm_get_data(spm_vol(dummy),xyz)';
+                %motion correction
+                common_pattern  = mean( roi.raw ,2);%average across conditions (but not across sub or group)
+                roi.mc          = roi.raw - repmat(common_pattern,[1 tc 1 1]);
+                % at this stage we have a cell array ROI, which stores beta values for
+                % all conditions, subjects and groups. Left/Right has to be distributed
+                % to different cells because the number of voxels is different, so it
+                % can not be stored in a matrix.
+                %         roi.raw is:
+                %         [voxel,condition,subject,group]
+                %         group 1: context
+                %         group 2: nocontext
+                %% compute the similarity using different metrics
+                for dtype = fieldnames(roi)';
+                    data     = roi.(dtype{1}); % data of one sub
                     c_metric = 0;
-                    for metric = metrics
-                       
+                    for metric = metrics                        
                         smat = [];
                         if any(strcmp(metric{1},{'cov' 'mean'}))
                             fh       = str2func(metric{1});%get the function handle
@@ -127,27 +121,26 @@ for nroi       = rois;%run across all ROIs, and fill up the similarity matrix
                                 smat = diag(smat); %puts means into a diagonal
                             end
                         elseif any(strcmp(metric{1},{'correlation' 'euclidean' 'seuclidean'}))
-                            try%not all machines have toolbox for pdist
+                            try %not all machines have toolbox for pdist
                                 smat     = squareform(pdist(data',metric{1})); % i do not get this line (and its not working)
                             end
                         end
                         if ~isempty(smat)
-                            %store 
-                            c_metric = c_metric + 1;
+                            %store
+                            c_metric             = c_metric + 1;
                             sim.metric{c_metric} = metric{1};
-                            sim.d.(dtype{1})(:,:,sub,group,nroi,lr,c_metric) = smat;
+                            sim.(dtype{1})(:,:,s_c,gr,nroi,lr,c_metric) = smat;
                         end
+                        
                     end
-                    
                 end
-            end
+            end            
         end
+        fprintf('\n');
     end
+    sim.name = mask.name{1};%store the name
 end
-%save the result.
-% save(sprintf('%s/simmatpain.mat',analysis_path),'sim')
-save(sprintf('%s/simmatcue.mat',analysis_path),'sim')
-toc
-datestr(now)
+fprintf('Analysis finished in %.2g minutes.\n',toc/60);
+
 
 
